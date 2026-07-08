d46c3e09fbe7a653f08e5d6020c48fec6853495c
zanacodeX <supuusandar@gmail.com>
2026-07-08 13:26:24 +0530

    Backend branch: remove frontend source and client-app

--- client-app/launch-client.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM === Pixel Art POS Client Launcher ===
-REM Change this to your server's IP address
-set SERVER_URL=http://localhost:3000
-
-start "" "%~dp0Pixel Art POS Client.exe" --server %SERVER_URL%
